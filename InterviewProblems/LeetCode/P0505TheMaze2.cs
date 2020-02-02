9ffd040293fc77ce589b6dded63f0d59fceddbba
jerrybarona <jab2397@columbia.edu>
2020-02-02 01:09:10 -0700

    The Maze2

---
+++ InterviewProblems/LeetCode/P0505TheMaze2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewProblems.LeetCode
+{
+    public class P0505TheMaze2
+    {
+        public void ShortestDistanceTest()
+        {
+            var maze = new[] { new[] { 0, 0, 0, 0, 1 }, new[] { 0, 0, 0, 0, 0 }, new[] { 0, 0, 0, 0, 0 }, new[] { 1, 1, 0, 0, 0 }, new[] { 1, 1, 0, 1, 1 } };
+            var start = new[] { 0, 0 };
+            var destination = new[] { 4, 2 };
+
+            Console.WriteLine(ShortestDistance(maze, start, destination));
+        }
+
+        public int ShortestDistance(int[][] maze, int[] start, int[] destination)
+        {
+            var m = maze.Length;
+            var n = maze[0].Length;
+            var memo = new Dictionary<(int, int), int>();
+            var distance = MinDist(start[0], start[1]);
+            return distance >= 200000 ? -1 : distance;
+
+            int MinDist(int r, int c)
+            {
+
+                if (r == destination[0] && c == destination[1]) return 0;
+                if (memo.ContainsKey((r, c))) return memo[(r, c)];
+                if (maze[r][c] == -1) return 200000;
+
+                maze[r][c] = -1;
+                var result = 200000;
+                var s = 0;
+                while (r + s + 1 < m && maze[r + s + 1][c] != 1) ++s;
+                result = Math.Min(result, s + MinDist(r + s, c));
+
+                s = 0;
+                while (r - s - 1 >= 0 && maze[r - s - 1][c] != 1) ++s;
+                result = Math.Min(result, s + MinDist(r - s, c));
+
+                s = 0;
+                while (c + s + 1 < n && maze[r][c + s + 1] != 1) ++s;
+                result = Math.Min(result, s + MinDist(r, c + s));
+
+                s = 0;
+                while (c - s - 1 >= 0 && maze[r][c - s - 1] != 1) ++s;
+                result = Math.Min(result, s + MinDist(r, c - s));
+                maze[r][c] = 0;
+
+                memo.Add((r, c), Math.Min(result, 200000));
+                return memo[(r, c)];
+            }
+        }
+    }
+}
---
+++ InterviewProblems/Microsoft/P026PrintBinaryTree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewProblems.Microsoft
+{
+    public class P026PrintBinaryTree
+    {
+        public void PrintBinaryTreeTest()
+        {
+            var root = new Node(1);
+            root.Left = new Node(2);
+            root.Right = new Node(3);
+            root.Left.Left = new Node(4);
+            root.Left.Right = new Node(5);
+            root.Right.Left = new Node(6);
+            root.Right.Right = new Node(7);
+            root.Left.Left.Left = new Node(8);
+            root.Left.Left.Right = new Node(9);
+            root.Left.Right.Left = new Node(1);
+            root.Left.Right.Right = new Node(2);
+            root.Right.Left.Left = new Node(3);
+            root.Right.Left.Right = new Node(4);
+            root.Right.Right.Left = new Node(5);
+            root.Right.Right.Right = new Node(6);
+
+            PrintBinaryTree(root);
+        }
+
+        public void PrintBinaryTree(Node root)
+        {
+            var levels = new List<List<(int val, int x)>>();
+            Traverse(root, 0, 0);
+            var coords = new Dictionary<(int, int),int>();
+
+            var offset = 1;
+            var step = 2;
+            var maxj = 0;
+            for (var i = levels.Count - 1; i >= 0; --i)
+            {                
+                foreach (var item in levels[i])
+                {
+                    var j = offset + (item.x) * step;
+                    coords.Add((i, j), item.val);
+                    maxj = Math.Max(maxj, j);
+                }
+
+                offset = step;
+                step *= 2;
+            }
+
+            for (var i = 0; i < levels.Count; ++i)
+            {
+                for (var j = 0; j <= maxj; ++j)
+                {
+                    if (coords.ContainsKey((i, j))) Console.Write(coords[(i, j)]);
+                    else Console.Write(' ');
+                }
+                Console.Write("\n");
+            }
+
+
+            void Traverse(Node node, int level, int x)
+            {
+                if (node == null) return;
+                if (level == levels.Count) levels.Add(new List<(int val, int x)>());
+                levels[level].Add((node.Value, x));
+
+                Traverse(node.Left, level + 1, (x << 1) + 0);
+                Traverse(node.Right, level + 1, (x << 1) + 1);
+            }
+        }
+
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
