487de19e80ebf1e6ea22c79f8b808c4c99301ded
Jerry Barona <Jerry.Fajardo@aexp.com>
2020-02-14 14:56:27 -0700

    K Empty Slots - leetcode

---
+++ InterviewProblems/LeetCode/P0683KEmptySlots.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewProblems.LeetCode
+{
+    public class P0683KEmptySlots
+    {
+        public void KEmptySlotsTest()
+        {
+            var bulbs = new[] { 3, 9, 2, 8, 1, 6, 10, 5, 4, 7 };
+            var k = 1;
+            Console.WriteLine(KEmptySlots(bulbs,k));
+        }
+
+        public int KEmptySlots(int[] bulbs, int K)
+        {
+            var days = bulbs.Select((bulb, day0) => (bulb, day0)).Aggregate(new int[bulbs.Length + 1], (arr, x) =>
+            {
+                arr[x.bulb] = x.day0 + 1;
+                return arr;
+            });
+
+            var window = new LinkedList<int>();
+
+            for (var i = 0; i < K; ++i)
+            {
+                while (window.Count > 0 && days[2 + i] <= days[window.Last.Value]) window.RemoveLast();
+                window.AddLast(2 + i);
+            }
+
+            var left = 1;
+            var right = left + K + 1;
+            var result = int.MaxValue;
+
+            for (; right < days.Length; ++left, ++right)
+            {
+                if (window.Count > 0 && left == window.First.Value) window.RemoveFirst();
+                var min = window.Count > 0 ? days[window.First.Value] : int.MaxValue;
+                if (days[left] < min && days[right] < min) result = Math.Min(result, Math.Max(days[left], days[right]));
+                while (window.Count > 0 && days[right] <= days[window.Last.Value]) window.RemoveLast();
+                window.AddLast(right);
+            }
+
+            return result == int.MaxValue ? -1 : result;
+        }
+    }
+}
---
+++ InterviewProblems/Oracle/P005SeachInEvenOddIndexSortedArray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace InterviewProblems.Oracle
+{
+    public class P005SeachInEvenOddIndexSortedArray
+    {
+        public void SearchInEvenOddIndexSortedArrayTest()
+        {
+            var arr = new[] { 10, 4, 6, 5, 5, 6, 2 };
+
+            Console.WriteLine(SearchInEvenOddIndexSortedArray(arr, 3));
+
+            var arr1 = new[] { 5, 2 };
+            Console.WriteLine(SearchInEvenOddIndexSortedArray(arr1, 3));
+        }
+
+        public int SearchInEvenOddIndexSortedArray(int[] arr, int target)
+        {
+            if (arr == null || arr.Length == 0) return -1;
+            if (arr.Length == 1) return arr[0] == target ? 0 : -1;
+
+            var len = arr.Length;
+            var estart = 0;
+            var ostart = 1;
+            var eend = (len - 1) % 2 == 0 ? len - 1 : len - 2;
+            var oend = (len - 1) % 2 == 1 ? len - 1 : len - 2;
+
+            // even index binary search
+
+            for (var mid = (estart + (eend - estart) / 2) % 2 == 0
+                     ? estart + (eend - estart) / 2
+                     : (estart + (eend - estart) / 2) - 1;
+                 estart <= eend;
+                 mid = (estart + (eend - estart) / 2) % 2 == 0
+                     ? estart + (eend - estart) / 2
+                     : (estart + (eend - estart) / 2) - 1)
+            {
+                if (arr[mid] == target) return mid;
+                if (target > arr[mid]) eend = mid - 2;
+                else estart = mid + 2;
+            }
+
+            // odd index binary search
+
+            for (var mid = (ostart + (oend - ostart) / 2) % 2 == 1
+                     ? ostart + (oend - ostart) / 2
+                     : (ostart + (oend - ostart) / 2) - 1;
+                 ostart <= oend;
+                 mid = (ostart + (oend - ostart) / 2) % 2 == 1
+                     ? ostart + (oend - ostart) / 2
+                     : (ostart + (oend - ostart) / 2) - 1)
+            {
+                if (arr[mid] == target) return mid;
+                if (target > arr[mid]) ostart = mid + 2;
+                else oend = mid - 2;
+            }
+
+            return -1;
+        }
+    }
+}
